5ba2ab127800139b10c7d5893bd1c535cc721c15
Sebastien SALIQUES <sebastien.saliques@gmail.com>
2020-05-28 15:19:57 +0200

    Ajout du gradient

--- JeuMathsCM/ViewController.swift
+++ JeuMathsCM/ViewController.swift
@@ -17,8 +17,22 @@ class ViewController: UIViewController {
     var hauteurCarte : CGFloat = 200
     var largeurCarte : CGFloat = 200
     
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let gradient = CAGradientLayer()
+        gradient.colors = [UIColor.black.cgColor, UIColor.darkGray.cgColor]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 1, y: 1)
+        gradient.opacity = 1
+        gradient.frame = view.bounds
+        view.layer.addSublayer(gradient)
+        view.bringSubviewToFront(container)
+        
+        
         
         carte = MaVue(frame: CGRect(x: container.frame.midX - (largeurCarte / 2), y: container.frame.midY - (hauteurCarte / 2), width: largeurCarte, height: hauteurCarte))
         container.addSubview(carte ?? UIView())
